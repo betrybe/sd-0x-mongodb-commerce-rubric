53147b83d5abe0e5daaeb950acaf5617b21d6ef0
LizzardMedeiros <luis.medeiros@outlook.com.br>
2021-03-19 10:35:50 -0300

    test

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,3 +1,6 @@
-db.produtos.updateMany({ nome: { $in: ['Big Mac', 'Quarteirão com Queijo'] } }, { $push: { ingredientes: 'bacon' } });
+db.produtos.updateMany(
+  { nome: { $in: ['Big Mac', 'Quarteirão com Queijo'] } },
+  { $push: { ingredientes: 'bacon' } },
+);
 
 db.produtos.find({}, { _id: false, nome: true, ingredientes: true });
