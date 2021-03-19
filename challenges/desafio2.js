c0cef0ce35cb30feccbb1fde499408ad32847508
LizzardMedeiros <luis.medeiros@outlook.com.br>
2021-03-19 10:41:56 -0300

    Have to pass

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
   { valorUnitario: { $exists: false } },
-  { $set: { valorUnitario: 0.00 } },
+  { $set: { valorUnitario: numberDecimal('0.00') } },
 );
 
 db.produtos.find({}, { _id: false, nome: true, valorUnitario: true });
